44796b6a132e4da47be8cc00820ed3c362b7ba1e
Houssam Nasir <77208956+BabaY4ga-Unow@users.noreply.github.com>
2023-12-30 18:43:49 +0100

    Surplus in the User Update Form

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrl: './settings.component.scss'
+  styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent {
+  userData = {
+    username: '',
+    email: '',
+    currentPassword: '',
+    newPassword: '',
+    confirmPassword: ''
+  };
 
+  updateSettings() {
+    // Perform your update logic here, e.g., making an API request to update the user's settings
+    // Once the update is successful, display a pop-up message and clear the form
+    this.showSuccessPopup();
+    this.clearForm();
+  }
+
+  showSuccessPopup() {
+    alert('Successfully updated!');
+  }
+
+  clearForm() {
+    this.userData = {
+      username: '',
+      email: '',
+      currentPassword: '',
+      newPassword: '',
+      confirmPassword: ''
+    };
+  }
 }
